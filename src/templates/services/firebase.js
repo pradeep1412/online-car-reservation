af67c1191bd04781b5f719feb07f61f7cfd9a0b4
pradeep1412 <61228637+pradeep1412@users.noreply.github.com>
2021-04-28 13:39:49 +0530

    Update firebase.js

--- src/templates/services/firebase.js
+++ src/templates/services/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "your are apikey",
-    authDomain: "your are ",
+    authDomain: "your are authDomain",
     projectId: "your are projectId",
     storageBucket: "your are storageBucket",
     messagingSenderId: "your are messagingSenderId",
